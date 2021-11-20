12fcd6cb1d460e6b20d3ed4446b574bf5c0be529
Gilbert Kim <gsk009@bucknell.edu>
2021-11-20 08:05:33 -0600

    Initial project creation

---
+++ ProfileCreation/ProfileCreation/ProfileCreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCreationApp.swift
+//  ProfileCreation
+//
+//  Created by Gilbert Kim on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
